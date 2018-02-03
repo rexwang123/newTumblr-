b3a9f33bc7e7ba064afdfd705d8011dba0cfa208
Jiayi Wang <jiayiwang@Jiayis-MacBook-Pro.local>
2018-02-03 11:47:20 -0800

    new change

---
+++ Tumblr/ TODO: Reload the table view  } PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//   TODO: Reload the table view  } PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Jiayi Wang on 2/2/18.
+//  Copyright © 2018 Jiayi Wang. All rights reserved.
+//
+
+import UIKit
+
+class _TODO__Reload_the_table_view____PhotosViewController: Tumblr {
+
+}
---
+++ Tumblr/View Controllers/PhotosViewController.swift
@@ -0,0 +1,81 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Jiayi Wang on 2/2/18.
+//  Copyright © 2018 Jiayi Wang. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+   
+    
+    var posts: [[String: Any]] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 5
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "This is row \(indexPath.row)"
+            
+            return cell
+        }
+        
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
