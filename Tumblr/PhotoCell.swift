be97188045c5485073ec3f08fb4fe7b727a53907
Jiayi Wang <jiayiwang@jiayis-mbp.attlocal.net>
2018-02-03 21:06:02 -0800

    new change

--- Tumblr/PhotoCell.swift
+++ Tumblr/PhotoCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PhotoCell: UITableViewCell {
 
+    @IBOutlet weak var userImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Tumblr/View Controllers/PhotosViewController.swift
+++ Tumblr/View Controllers/PhotosViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
@@ -57,9 +58,22 @@ class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
         cell.textLabel?.text = "This is row \(indexPath.row)"
-        
+       
+        let post = posts[indexPath.row]
+       
+        if let photos = post["photos"] as? [[String: Any]] {
+            // 1.
+            let photo = photos[0]
+            // 2.
+            let originalSize = photo["original_size"] as! [String: Any]
+            // 3.
+            let urlString = originalSize["url"] as! String
+            // 4.
+            let url = URL(string: urlString)
+            // TODO: Get the photo url
+        }
         return cell
     }
     
