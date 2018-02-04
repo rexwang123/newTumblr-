528a6f2070203ed8db2fc4aa5a4a1fce647c7320
Jiayi Wang <jiayiwang@jiayis-mbp.attlocal.net>
2018-02-03 19:01:20 -0800

    new change

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Jiayi Wang on 2/3/18.
+//  Copyright © 2018 Jiayi Wang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Tumblr/View Controllers/PhotosViewController.swift
+++ Tumblr/View Controllers/PhotosViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
    
+    
     @IBOutlet weak var tableView: UITableView!
     
-   
+    @IBOutlet weak var PhotoCell: PhotoCell!
+    
+    
     
     var posts: [[String: Any]] = []
     override func viewDidLoad() {
@@ -23,17 +26,6 @@ class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDe
         tableView.delegate = self
         tableView.dataSource = self
         
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 5
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = UITableViewCell()
-            cell.textLabel?.text = "This is row \(indexPath.row)"
-            
-            return cell
-        }
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
@@ -60,6 +52,17 @@ class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDe
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
@@ -67,7 +70,8 @@ class PhotosViewController: UIViewController,UITableViewDataSource,UITableViewDe
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+  
     /*
     // MARK: - Navigation
 
